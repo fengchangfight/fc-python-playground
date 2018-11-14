c3af605a4767a81a2fc2354706af41ca0c2bdf0c
fengchang <fengchang_fight@163.com>
2018-11-14 12:56:12 +0800

    file ops with try except

---
+++ fileops.py
@@ -0,0 +1,15 @@
+
+try:
+    f = open("/tmp/test2.txt", 'r')
+    content = f.readlines()
+    print(content)
+except IOError as e:
+    print(e)
+
+#
+# f = open("/tmp/test2.txt", 'r')
+# content = f.readlines()
+# print(content)
+
+
+print("end program")
